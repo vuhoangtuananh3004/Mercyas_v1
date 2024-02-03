3a1d420d000f788f8ca143a4f3927c4325e8b142
pizzatowns <pizzatownsongo@gmail.com>
2024-02-02 21:47:23 -0500

    nativewindcss

--- front_end/App.js
+++ front_end/App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center items-center bg-blue-200">
+      <Text className="text-xl font-semibold text-red-500">Welcome to NativeWind</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
