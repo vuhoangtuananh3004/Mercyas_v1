9171a2a40ccda8a308d9bd3fadbc1e52029cbf15
Thanh Pham <thanhphamlike@gmail.com>
2024-02-06 21:52:03 -0500

    added scan template

--- front_end/components/ScanReceipt/SwitchButton.js
+++ front_end/components/ScanReceipt/SwitchButton.js
@@ -11,7 +11,7 @@ function SwitchButton() {
     <View className="relative mt-10">
       <View className="flex flex-row justify-around items-center h-[36px] w-[260px] border-2 border-black rounded-[24px]">
         <Text className="text-xl font-bold">Scan</Text>
-        <Text className="text-xl font-bold">Manually</Text>
+        <Text className="text-xl font-bold">Manual</Text>
       </View>
       <TouchableOpacity className={`absolute transition duration-400 delay-200 ${!isSwitch ? '' : 'translate-x-[130px]'}  bg-green h-full w-[130px] bg-green-400/40 rounded-[24px]`} onPress={switchButton}/>
     </View>
--- front_end/components/pages/Scan.js
+++ front_end/components/pages/Scan.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
-// import { styled } from 'nativewind'
-import ScanToggleButton from '../ScanToggleButton'
-import ManualToggleButton from '../ManualToggleButton'
 import SwitchButton from '../ScanReceipt/SwitchButton'
-
-// const StyledView = styled(SafeAreaView)
-// const StyledText =  styled(Text)
+import Checkbox from 'expo-checkbox';
 
 const Scan = () => {
+    const [isStore, setStore] = useState(false);
+    const [isDate, setDate] = useState(false);
+    const [isTotal, setTotal] = useState(false);
     return (
-        <SafeAreaView className="flex flex-row">
-            {/* <ScanToggleButton></ScanToggleButton>
-            <ManualToggleButton></ManualToggleButton> */}
-            <SwitchButton/>
+        <SafeAreaView className="flex items-center">
+            <SwitchButton />
+
+            <View className="px-2 mt-4">
+                <Text className="text-center">
+                    Kindly position the receipt within the designated frame
+                </Text>
+            </View>
+
+            <View className="px-2 mt-4 w-4/5 h-4/6 border border-sky-500">
+                <Text className>This is the frame</Text>
+            </View>
+            <View className="flex flex-row w-full justify-evenly mt-4">
+                <View className="flex flex-row gap-2">
+                    <Checkbox value={isStore} onValueChange={setStore} />
+                    <Text>Store</Text>
+                </View>
+                <View className="flex flex-row gap-2">
+                    <Checkbox value={isDate} onValueChange={setDate} />
+                    <Text>Date</Text>
+                </View>
+                <View className="flex flex-row gap-2">
+                    <Checkbox value={isTotal} onValueChange={setTotal} />
+                    <Text>Total</Text>
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
