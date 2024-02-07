d075ac23fafe3053ff72def4d22c2a07f548b8e5
Thinh <50585271+phamthinh14@users.noreply.github.com>
2024-02-07 13:58:45 -0500

    - Added Registration page

--- front_end/components/LoginComp/Login.js
+++ front_end/components/LoginComp/Login.js
@@ -3,6 +3,7 @@ import { View, Text, Image, TextInput, Button } from "react-native";
 import SloganAndImageComp from "./SloganAndLogo";
 import EmailLogin from "./EmailLogin";
 import PhoneNumsVerification from "./PhoneNumsVerification";
+import RegistrationComp from "./RegistrationComp";
 
 export default function Login() {
     return (
@@ -10,7 +11,7 @@ export default function Login() {
             <View classname="flex flex-col">
                 <SloganAndImageComp />
                 {/* <EmailLogin /> */}
-                <PhoneNumsVerification />
+                <RegistrationComp />
             </View>
         </View>
     );
