a772f2496761c32f66c6103ce0c3e607fdce4a9c
Thanh Pham <thanhphamlike@gmail.com>
2024-02-07 22:47:53 -0500

    added home page structure

--- front_end/components/Camera/Camera.js
+++ front_end/components/Camera/Camera.js
@@ -1,5 +1,6 @@
-import {Text, View } from 'react-native'
-import React from 'react'
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Camera = () => {
   return (
---
+++ front_end/components/graphs/LineChart.js
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const LineChart = () => {
+    return (
+        <View className="border border-indigo-500 flex-1">
+            <View>
+                <Text>Graph section</Text>
+            </View>
+            <View>
+                <Text>Infomation section</Text>
+            </View>
+        </View>
+    )
+}
+
+export default LineChart
