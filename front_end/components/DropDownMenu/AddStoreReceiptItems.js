d38847667795e2d6c0543e945b68e4bcfc077885
pizzatowns <pizzatownsongo@gmail.com>
2024-02-12 17:31:12 -0500

    update

--- front_end/components/DropDownMenu/AddStoreReceiptItems.js
+++ front_end/components/DropDownMenu/AddStoreReceiptItems.js
@@ -27,24 +27,27 @@ export default function AddStoreReceiptItems() {
           <Text className="text-center">Del</Text>
         </View>
       </View>
-
-      <View className="flex flex-row h-[36px] items-center w-full">
-        <View className="w-[60px] text-center border-r-2 border-gray-200">
-          <Text className="text-center">1</Text>
-        </View>
-        <View className="w-[120px] text-center border-r-2 border-gray-200">
-          <Text className="text-center">Nike socks</Text>
-        </View>
-        <View className="w-[60px] text-center border-r-2 border-gray-200">
-          <Text className="text-center">2</Text>
-        </View>
-        <View className="w-[60px] text-center border-r-2 border-gray-200">
-          <Text className="text-center">22.44$</Text>
-        </View>
-        <View className="w-[60px] text-center">
-          <Text className="text-center">X</Text>
-        </View>
-      </View>
+      <ScrollView className='h-[215px]' >
+        {Array.from({ length: 20 }).map((_, index) => (
+          <View className={`flex flex-row h-[36px] items-center w-full ${(index % 2 == 0) ? 'bg-red-200/40' : 'bg-blue-200/40'}`}>
+            <View className="w-[60px] text-center border-r-2 border-gray-200">
+              <Text className="text-center">1</Text>
+            </View>
+            <View className="w-[120px] text-center border-r-2 border-gray-200">
+              <Text className="text-center">Nike socks</Text>
+            </View>
+            <View className="w-[60px] text-center border-r-2 border-gray-200">
+              <Text className="text-center">2</Text>
+            </View>
+            <View className="w-[60px] text-center border-r-2 border-gray-200">
+              <Text className="text-center">22.44$</Text>
+            </View>
+            <View className="w-[60px] text-center">
+              <Text className="text-center">X</Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
--- front_end/components/Test/Test.js
+++ front_end/components/Test/Test.js
@@ -1,19 +1,11 @@
 import React from "react";
 import { View, Text, Image, TextInput, Button } from "react-native";
-import SloganAndImageComp from "./SloganAndLogo";
-import EmailLogin from "./EmailLogin";
-import PhoneNumsVerification from "./PhoneNumsVerification";
-import RegistrationComp from "./RegistrationComp";
 
-export default function Login() {
-  // <View classname="flex flex-col">
-  // <SloganAndImageComp />
-  // {/* <EmailLogin /> */}
-  // <RegistrationComp />
-  // </View>
+
+export default function Test() {
   return (
-    <View classname="flex-1 h-10 w-10  bg-black">
-      <Text>asdsa</Text>
+    <View classname="flex-1 justify-center items-center ">
+      <Text>asdsasdasda</Text>
     </View>
   );
 }
