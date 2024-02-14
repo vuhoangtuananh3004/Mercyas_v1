566cc49f6e78b6c9585da98ac78ece7ab8cd04fb
pizzatowns <pizzatownsongo@gmail.com>
2024-02-13 20:49:06 -0500

    update

--- front_end/App.js
+++ front_end/App.js
@@ -4,13 +4,14 @@ import Manually from "./components/ScanReceipt/Manually";
 import ScanReceipt from "./components/ScanReceipt/ScanReceipt";
 import Footer from "./components/Footer/Footer";
 import Login from "./components/LoginComp/Login"
+import Home from "./components/pages/Home";
 
 export default function App() {
   // <ScanReceipt />
   // <Footer />
   return (
     <View className="flex-1">
-      <Login/>
+      <Home/>
     </View>
   );
 }
