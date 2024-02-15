4239312a81d5bebcca6a437cfec37999a3150ff2
Thanh Pham <thanhphamlike@gmail.com>
2024-02-15 14:10:40 -0500

    added transition between manual data input and automatic scan in Scan.js

--- front_end/screens/Scan.js
+++ front_end/screens/Scan.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import SwitchButton from '../components/ScanReceipt/SwitchButton'
 import Checkbox from 'expo-checkbox';
 import MyCamera from '../components/Camera/MyCamera';
-
+import Manually from '../components/ScanReceipt/Manually';
 const Scan = () => {
     const [isStore, setStore] = useState(false);
     const [isDate, setDate] = useState(false);
     const [isTotal, setTotal] = useState(false);
+    const [isManual, setManual] = useState(false);
+
+    useEffect(() => {
+        console.log("isMnaual: ", isManual);
+    }, [isManual])
+
+    onWitchClick = () => {
+        setManual(!isManual)
+    }
     return (
         <SafeAreaView className="flex-1 items-center">
-            <SwitchButton />
+            <SwitchButton onClickManual={onWitchClick} />
 
             <View className="px-2 mt-4">
                 <Text className="text-center">
@@ -18,23 +27,30 @@ const Scan = () => {
                 </Text>
             </View>
 
-            <View className=" mt-4 w-4/5 h-4/6 ">
-                <MyCamera />
-            </View>
-            <View className="flex flex-row w-full justify-evenly mt-4">
-                <View className="flex flex-row gap-2">
-                    <Checkbox value={isStore} onValueChange={setStore} />
-                    <Text>Store</Text>
+            {isManual ?
+                <Manually />
+                :
+                <View className=" mt-4 w-4/5 h-4/6 ">
+                    <MyCamera />
+
+                    <View className="flex flex-row w-full justify-evenly mt-4">
+                        <View className="flex flex-row gap-2">
+                            <Checkbox value={isStore} onValueChange={setStore} />
+                            <Text>Store</Text>
+                        </View>
+                        <View className="flex flex-row gap-2">
+                            <Checkbox value={isDate} onValueChange={setDate} />
+                            <Text>Date</Text>
+                        </View>
+                        <View className="flex flex-row gap-2">
+                            <Checkbox value={isTotal} onValueChange={setTotal} />
+                            <Text>Total</Text>
+                        </View>
+                    </View>
                 </View>
-                <View className="flex flex-row gap-2">
-                    <Checkbox value={isDate} onValueChange={setDate} />
-                    <Text>Date</Text>
-                </View>
-                <View className="flex flex-row gap-2">
-                    <Checkbox value={isTotal} onValueChange={setTotal} />
-                    <Text>Total</Text>
-                </View>
-            </View>
+            }
+
+
         </SafeAreaView>
     )
 }
