06bac75464d0449af2fd163f770e56a1111a90a2
Thanh Pham <thanhphamlike@gmail.com>
2024-02-20 22:12:18 -0500

    fixed camera layout

--- front_end/components/Camera/MyCamera.js
+++ front_end/components/Camera/MyCamera.js
@@ -32,34 +32,34 @@ const MyCamera = () => {
 
   if (showCamera) {
     return (
-      <View className="flex-1 items-center">
-        <View className="flex-1 flex-row">
+      <View className="flex-1 items-center gap-y-2">
+        <View className="flex-1 flex-row border rounded-xl p-1 bg-black">
           <Camera
             ref={ref => setCamera(ref)}
-            className="flex-1 rounded-xl"
+            className="flex-1"
             type={CameraType.back}
             ratio={'1:2'} />
         </View>
 
-        <TouchableOpacity className="bg-gray-300 my-2 border border-sky-500 rounded-xl" onPress={() => takePicture()}>
+        <TouchableOpacity className="bg-gray-300 border border-sky-500 rounded-xl" onPress={() => takePicture()}>
           <Text className="p-1" >Take Picture</Text>
         </TouchableOpacity>
-        
+
       </View>
     )
   } else {
     return (
-      <View className="flex-1 items-center">
-        <View className="grow h-4/5">
-          {image ? <Image className="flex-1" source={{ uri: image }} /> : <Text>None</Text>}
+      <View className="flex-1 items-center gap-y-2">
+        <View className="flex-1 w-full p-1 border rounded-xl bg-black">
+          {image ? <Image className="flex-1" source={{ uri: `${image}` }} /> : <Text>None</Text>}
         </View>
 
         {/* <TouchableOpacity title="Take Picture" onPress={() => takePicture()} /> */}
-        <View className="flex-1 flex-row gap-8">
-          <TouchableOpacity className="bg-gray-300 my-2 border border-sky-500 rounded-xl w-14 items-center" onPress={() => retakePicture()}>
+        <View className="flex-row gap-x-4">
+          <TouchableOpacity className="bg-gray-300 border border-sky-500 rounded-xl w-14 items-center" onPress={() => retakePicture()}>
             <Text className="p-1" >Retake</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="bg-gray-300 my-2 border border-sky-500 rounded-xl w-14 items-center">
+          <TouchableOpacity className="bg-gray-300 border border-sky-500 rounded-xl w-14 items-center">
             <Text className="p-1" >Done</Text>
           </TouchableOpacity>
         </View>
