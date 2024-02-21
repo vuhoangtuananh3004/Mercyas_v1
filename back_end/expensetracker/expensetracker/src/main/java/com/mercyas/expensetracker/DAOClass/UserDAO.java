889ba111544d4b68572eebc4f11b3674e446b5e2
Thinh <50585271+phamthinh14@users.noreply.github.com>
2024-02-21 14:31:18 -0500

    Proof of concept for back_end

--- back_end/expensetracker/expensetracker/src/main/java/com/mercyas/expensetracker/DAOClass/UserDAO.java
+++ back_end/expensetracker/expensetracker/src/main/java/com/mercyas/expensetracker/DAOClass/UserDAO.java
@@ -1,4 +1,7 @@
 package com.mercyas.expensetracker.DAOClass;
 
-public interface UserI {
+import com.mercyas.expensetracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User, Long> {
 }
